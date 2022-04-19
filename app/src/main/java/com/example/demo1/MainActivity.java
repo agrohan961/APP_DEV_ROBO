a2f402f418a425aa9cf72487d664440a7614180d
agrohan961 <ag.rohan961@gmail.com>
2022-04-19 22:51:27 +0530

    FINAL APP

---
+++ app/src/main/java/com/example/demo1/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.demo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView image;
+
+    int[] pics = {R.drawable.img, R.drawable.img_3, R.drawable.img_4, R.drawable.img_5};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        image = (ImageView) findViewById(R.id.picture);
+    }
+
+    public void change(View view)
+    {
+         Random rand = new Random();
+         int rand_int = rand.nextInt(4);
+         image.setImageResource(pics[rand_int]);
+    }
+}
